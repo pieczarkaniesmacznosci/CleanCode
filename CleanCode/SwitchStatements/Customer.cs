20c06f6c8fc9b71e81908acb8980e08cc5ee8f30
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-11-10 18:33:06 +0100

    Creation of Abstract classes to use polymorphism to calculate the statement

--- CleanCode/SwitchStatements/Customer.cs
+++ CleanCode/SwitchStatements/Customer.cs
@@ -1,37 +1,28 @@
-﻿
-using System;
-
-namespace CleanCode.SwitchStatements
+﻿namespace CleanCode.SwitchStatements
 {
-    public class Customer
+    public abstract class Customer
     {
-        public CustomerType Type { get; set; }
-
-        public MonthlyStatement GenerateStatement(MonthlyUsage monthlyUsage)
+        public abstract MonthlyStatement GenerateStatement(MonthlyUsage monthlyUsage);
+    }
+    public class PayAsYouGoCustomer : Customer
+    {
+        public override MonthlyStatement GenerateStatement(MonthlyUsage monthlyUsage)
         {
             var statement = new MonthlyStatement();
-            switch (Type)
-            {
-                case CustomerType.PayAsYouGo:
-                    statement.CallCost = 0.12f * monthlyUsage.CallMinutes;
-                    statement.SmsCost = 0.12f * monthlyUsage.SmsCount;
-                    statement.TotalCost = statement.CallCost + statement.SmsCost;
-                    break;
-
-                case CustomerType.Unlimited:
-                    statement.TotalCost = 54.90f;
-                    break;
-
-                default:
-                    throw new NotSupportedException("The current customer type is not supported");
-            }
+            statement.CallCost = 0.12f * monthlyUsage.CallMinutes;
+            statement.SmsCost = 0.12f * monthlyUsage.SmsCount;
+            statement.TotalCost = statement.CallCost + statement.SmsCost;
             return statement;
         }
     }
 
-    public enum CustomerType
+    public class UnlimitedCustomer : Customer
     {
-        PayAsYouGo = 1,
-        Unlimited
+        public override MonthlyStatement GenerateStatement(MonthlyUsage monthlyUsage)
+        {
+            var statement = new MonthlyStatement();
+            statement.TotalCost = 54.90f;
+            return statement;
+        }
     }
 }
