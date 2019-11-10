893a394794737eef81b5efd387533854d2f47a1d
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-11-10 17:57:59 +0100

    small refactor

--- CleanCode/SwitchStatements/MonthlyUsage.cs
+++ CleanCode/SwitchStatements/MonthlyUsage.cs
@@ -8,23 +8,25 @@ namespace CleanCode.SwitchStatements
         public int CallMinutes { get; set; }
         public int SmsCount { get; set; }
 
-        public void Generate(MonthlyStatement monthlyStatement)
+        public MonthlyStatement GenerateStatement()
         {
+            var statement = new MonthlyStatement();
             switch (Customer.Type)
             {
                 case CustomerType.PayAsYouGo:
-                    monthlyStatement.CallCost = 0.12f * CallMinutes;
-                    monthlyStatement.SmsCost = 0.12f * SmsCount;
-                    monthlyStatement.TotalCost = monthlyStatement.CallCost + monthlyStatement.SmsCost;
+                    statement.CallCost = 0.12f * CallMinutes;
+                    statement.SmsCost = 0.12f * SmsCount;
+                    statement.TotalCost = statement.CallCost + statement.SmsCost;
                     break;
 
                 case CustomerType.Unlimited:
-                    monthlyStatement.TotalCost = 54.90f;
+                    statement.TotalCost = 54.90f;
                     break;
 
                 default:
                     throw new NotSupportedException("The current customer type is not supported");
             }
+            return statement;
         }
     }
 }
